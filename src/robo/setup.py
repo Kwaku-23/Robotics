aa7efe0dd7b022b6f629660ae7c288f0094a3f86
Kwaku Asante Yeboah <yeboahasantekwaku@gmail.com>
2026-02-01 00:53:28 +0000

    Initial ROS2 Jazzy workspace with source packages

---
+++ src/robo/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'robo'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kwaku',
+    maintainer_email='kwaku@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            "test_node = robo.my_first_node:main",
+            "number_publisher = robo.number_publisher:main"
+        ],
+    },
+)
